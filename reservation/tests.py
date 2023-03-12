87b059b3eaefaf25b0d19f223c82b0b969bc3867
SEbastianSpin <sebasespinalejo@gmail.com>
2023-03-12 15:40:40 +0100

    now route / send the html response

--- reservation/tests.py
+++ reservation/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 class MyViewTestCase(TestCase):
     def test_my_view(self):
-        url = reverse('my_view')
+        url = reverse('')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<h1>Hello, world!</h1>')
--- reservation/urls.py
+++ reservation/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('my-view/', views.my_view, name='my_view'),
+    path('', views.my_view, name='my_view'),
 ]
