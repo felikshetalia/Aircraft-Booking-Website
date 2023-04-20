bd31d9adbf700de10c0dfae5e8f66e0204dcea3b
Dipankar Purecha <dipupurecha01@gmail.com>
2023-04-20 14:07:10 +0200

    Move aircraft images to media/aircrafts from aircrafts and certificate images to media/certificates from certificates

--- reservation/tests.py
+++ reservation/tests.py
@@ -11,8 +11,8 @@ class MyViewTestCase(TestCase):
 
 class MyPlaneTests(TestCase):
     def setUp(self):
-        Aircraft.objects.create(id="1", currentSpeed="10" ,model ="Cesna" , fuelCapacity=100 , timeForInspection="2023-04-26" , description="Nice test" , image="aircrafts/goat.jpg", availability=True)
-        Aircraft.objects.create(id="2", currentSpeed="10" ,model ="Cesna" , fuelCapacity=90 , timeForInspection="2023-04-25" , description="Nice test 2" , image="aircrafts/goat.jpg", availability=True)
+        Aircraft.objects.create(id="1", currentSpeed="10", model ="Cesna", fuelCapacity=100, timeForInspection="2023-04-26", description="Nice test", image="media/pics/oip.jpg", availability=True)
+        Aircraft.objects.create(id="2", currentSpeed="10", model ="Cesna", fuelCapacity=90, timeForInspection="2023-04-25", description="Nice test 2", image="media/pics/oip (1).jpg", availability=True)
     def test_PlaneCreation(self):
         count =Aircraft.objects.count()
         self.assertNotEqual(count, 0)
