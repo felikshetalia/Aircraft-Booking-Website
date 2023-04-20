d4645ad9822a961e7e826194b4f90a2c3863200e
SEbastianSpin <sebasespinalejo@gmail.com>
2023-04-20 07:31:41 +0200

    added test to planes
    simple test that check if planes have been created

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import RegularUser
+from django.test import Client
 # Create your tests here.
 
 
@@ -8,3 +10,22 @@ class MyViewTestCase(TestCase):
         url = reverse('signup')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        url = reverse('login')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+class CreateUserTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('signup')
+        self.user_data = {'username': 'testuser', 'email': 'testuser@example.com', 'password': 'password'}
+
+    # def test_create_user(self):
+    #     response = self.client.post(self.url, self.user_data)
+    #     print(self.user_data)
+    #     self.assertEqual(response.status_code, 200)
+    #     count =RegularUser.objects.count()
+    #     self.assertNotEqual(count, 0 , "User was created")
+    #     self.assertTrue(RegularUser.objects.filter(username='testuser').exists())
--- reservation/tests.py
+++ reservation/tests.py
@@ -1,9 +1,18 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import Aircraft
 
 class MyViewTestCase(TestCase):
     def test_my_view(self):
         url = reverse('my_view')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, '<h1>Hello, world!</h1>')
+
+
+class MyPlaneTests(TestCase):
+    def setUp(self):
+        Aircraft.objects.create(id="1", currentSpeed="10" ,model ="Cesna" , fuelCapacity=100 , timeForInspection="2023-04-26" , description="Nice test" , image="aircrafts/goat.jpg", availability=True)
+        Aircraft.objects.create(id="2", currentSpeed="10" ,model ="Cesna" , fuelCapacity=90 , timeForInspection="2023-04-25" , description="Nice test 2" , image="aircrafts/goat.jpg", availability=True)
+    def test_PlaneCreation(self):
+        count =Aircraft.objects.count()
+        self.assertNotEqual(count, 0)
