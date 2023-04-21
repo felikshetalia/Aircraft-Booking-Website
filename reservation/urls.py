09e98565e40e11f8f7e5fc4cac90ca9584f9b66c
SEbastianSpin <sebasespinalejo@gmail.com>
2023-04-21 04:27:36 +0200

    solved boostrap bug it was not working via CDN

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import  PlanesView
 
 urlpatterns = [
     path('', views.my_view, name='my_view'),
+     path('planes/',PlanesView.as_view(),name='planes')
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+
+class PlanesView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('planes')
+    template_name = 'planes/planeslist.html'
 
 def my_view(request):
     return HttpResponse("<html><body><h1>Hello, world!</h1></body></html>")
+
+
+
