828a0eb6931fe2fd65f5b56721f617a7fbb459d1
SEbastianSpin <sebasespinalejo@gmail.com>
2023-05-01 16:01:54 +0200

    added eng page set up

--- accounts/views.py
+++ accounts/views.py
@@ -12,3 +12,9 @@ class SignUpView(generic.CreateView):
     template_name = 'registration/signup.html'
 
 
+class EngineerView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('engineer')
+    template_name = 'engineer/engineer.html'
+
+
