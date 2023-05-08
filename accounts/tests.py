1a8a84c704d1a9e32fcec0d3b08e607720a3800d
SEbastianSpin <sebasespinalejo@gmail.com>
2023-05-08 15:22:50 +0200

    add test for new pages, sigup, booking creation

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import RegularUser
 from django.test import Client
+from django.contrib.auth import get_user_model
+from .forms import UserSignUpForm
 # Create your tests here.
 
 
@@ -16,11 +17,32 @@ class MyViewTestCase(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-class CreateUserTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.url = reverse('signup')
-        self.user_data = {'username': 'testuser', 'email': 'testuser@example.com', 'password': 'password'}
+
+User = get_user_model()
+
+class UserSignUpFormTestCase(TestCase):
+    def test_save_method(self):
+        form_data = {
+            'username': 'testuser',
+            'password1': 'NotEasyPassword124^',
+            'password2': 'NotEasyPassword124^',
+            'email': 'testuser@example.com',
+        }
+        form = UserSignUpForm(data=form_data)
+        if not form.is_valid():
+            print(form.errors)
+        self.assertTrue(form.is_valid())
+        user = form.save()
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(user.username, 'testuser')
+        self.assertTrue(user.is_user)
+
+
+# class CreateUserTestCase(TestCase):
+#     def setUp(self):
+#         self.client = Client()
+#         self.url = reverse('signup')
+#         self.user_data = {'username': 'testuser', 'email': 'testuser@example.com', 'password': 'password'}
 
     # def test_create_user(self):
     #     response = self.client.post(self.url, self.user_data)
